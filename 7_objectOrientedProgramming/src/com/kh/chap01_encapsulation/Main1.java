420da07052323cdffd34948e8ca0a8dc43e6e888
user2 <user2@KH_C>
2019-01-18 21:26:36 +0900

    실습 2

---
+++ 7_objectOrientedProgramming/src/com/kh/chap01_encapsulation/Main1.java
@@ -0,0 +1,12 @@
+package com.kh.chap01_encapsulation;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		String str = null;
+		String str2 = "asd";
+		
+		System.out.println(str2.equals(str));
+	}
+
+}
